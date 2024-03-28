5e6292dbaad8e29b3aedf3984b44ee6292c0baca
Prosper Atu <atu.prosper@gmail.com>
2024-03-28 09:54:13 +0100

    task 7

--- binary_trees.h
+++ binary_trees.h
@@ -9,18 +9,19 @@
 /* Data structures */
 
 /**
- * struct binary_tree_s - Binary tree node
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+* struct binary_tree_s - Binary tree node
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
 struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
+int n;
+
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
@@ -28,19 +29,22 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /**
- * struct loader_queue_s - struct for ordering traversal queue by level.
- * @node: A node in the binary tree.
- * @next: The next node to traverse to in the binary tree.
- */
+* struct loader_queue_s - struct for ordering traversal queue by level.
+* @node: A node in the binary tree.
+* @next: The next node to traverse to in the binary tree.
+*/
 typedef struct loader_queue_s
 {
-  binary_tree_t *node;
-  struct loader_queue_s *next;
+binary_tree_t *node;
+struct loader_queue_s *next;
 } levelorder_queue_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
@@ -78,7 +82,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
 void binary_tree_print(const binary_tree_t *);
 
 #endif
